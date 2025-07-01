048f673ade5123a2d86c2bf14c200f5db0b5565c
Agnishwar Raychaudhuri <157399810+agnishwarr@users.noreply.github.com>
2025-07-01 13:26:42 +0530

    Time: 45 ms (63.85%) | Memory: 14.4 MB (55.05%) - LeetSync

---
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findLHS(self, nums):
+        count = Counter(nums)
+        max_len=0
+
+        for num in count:
+            if num+1 in count:
+                max_len = max(max_len, count[num]+count[num+1])
+        return max_len
