48a62b64ba41a224767c41796f94e630934b8377
Agnishwar Raychaudhuri <157399810+agnishwarr@users.noreply.github.com>
2025-07-09 14:00:32 +0530

    Time: 43 ms (40.76%) | Memory: 18 MB (48.02%) - LeetSync

---
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        count = Counter(tiles)
+        
+        def dfs(counter):
+            total = 0
+            for ch in counter:
+                if counter[ch] > 0:
+                    total += 1
+                    counter[ch] -= 1
+                    total += dfs(counter)
+                    counter[ch] += 1
+            return total
+        
+        return dfs(count)
